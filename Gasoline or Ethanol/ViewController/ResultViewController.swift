8e47334602faaad6c11b50e8a660be3050fbb754
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-07-29 12:25:22 -0300

    🎨 Style: Added Main Screen, Form Screen and Result Screen

---
+++ Gasoline or Ethanol/ViewController/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  Gasoline or Ethanol
+//
+//  Created by Luiz Henrique Silva Sampaio on 29/07/23.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
